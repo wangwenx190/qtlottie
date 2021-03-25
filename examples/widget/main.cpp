61233e853a4ac2f85752163872b4a352e0a2758e
Yuhang Zhao <2546789017@qq.com>
2021-03-25 14:42:52 +0800

    Fix compilation error caused by typo
    Signed-off-by: Yuhang Zhao <2546789017@qq.com>

--- examples/widget/main.cpp
+++ examples/widget/main.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char *argv[]) {
 #if (QT_VERSION < QT_VERSION_CHECK(6, 0, 0))
-    QGuiApplication::setAttribute(Qt::AA_EnabledHighDpiScaling);
+    QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QGuiApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
 #if (QT_VERSION >= QT_VERSION_CHECK(5, 14, 0))
     QGuiApplication::setHighDpiScaleFactorRoundingPolicy(Qt::HighDpiScaleFactorRoundingPolicy::PassThrough);
